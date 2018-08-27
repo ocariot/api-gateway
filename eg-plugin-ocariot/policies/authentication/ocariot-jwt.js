d7d8abb764d833457828937d228f8dbd7662ac09
Alex Fernandes Figueirêdo <alex.figueiredo@ee.ufcg.edu.br>
2018-08-27 17:01:42 -0300

    Politicas de autenticacao e validacao funcionando com todas as validacoes: expiracao, issuer, assinatura, escopo, usuario valido e ativo

--- eg-plugin-ocariot/policies/authentication/ocariot-jwt.js
+++ eg-plugin-ocariot/policies/authentication/ocariot-jwt.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const passportJWT = require("passport-jwt");
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
+const services = require('express-gateway/lib/services');
 
 module.exports = function (actionParams) {
 
@@ -11,19 +12,32 @@ module.exports = function (actionParams) {
         jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
         secretOrKey: secretOrKey,
         issuer: actionParams.issuer
-    }, (jwtPayload, cb) => {
-
+    }, (jwtPayload, done) => {
+        //At this point both the jwt signature, issuer and experation were validated
+        // Futhermore, we have the jwt payload decoded and we can access its attributes
         console.log(`JWT payload: ${JSON.stringify(jwtPayload)}`);
 
-        //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
-        var err = new Error('Authentication failed');
-        err.status = 400;
-        return cb(null,jwtPayload);
+        //User validation. We expect to receive the username in the jwt 'sub' field
+        if (!jwtPayload.sub) {
+            return done(null, false);
+        }
+        services.auth.validateConsumer(jwtPayload.sub, { checkUsername: true })
+            .then((consumer) => {
+                if (!consumer) {
+                    return done(null, false); //invalid username or inctive user
+                }
+                return done(null, jwtPayload); //jwt successfully authenticated
+            }).catch((err) => {
+                if (err.message === 'CREDENTIAL_NOT_FOUND') {
+                    return done(null, false);
+                }
+                return done(err);
+            });       
     }
     ));
 
     return (req, res, next) => {
         console.log('executing ocariot authentication policy with params', actionParams);
-        passport.authenticate('jwt', {session: false})(req, res, next);
+        passport.authenticate('jwt', { session: false })(req, res, next);
     }
 };
