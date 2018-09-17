38405ae0952408b7af28906ab871e417dd30c6d3
Alex Fernandes Figueirêdo <alex.figueiredo@ee.ufcg.edu.br>
2018-09-17 19:01:46 -0300

    Updating Gateway custom endpoints Adding users router to ocariot plugin in order to expose routes to register and delete user.

--- eg-plugin-ocariot/manifest.js
+++ eg-plugin-ocariot/manifest.js
@@ -2,7 +2,9 @@ module.exports = {
   version: '1.0.0',
   init: function (pluginContext) {
     pluginContext.registerPolicy(require('./policies/authentication/ocariot-jwt-policy'));
-    pluginContext.registerPolicy(require('./policies/authorization/ocariot-jwtScopes-policy'));	
+    pluginContext.registerPolicy(require('./policies/authorization/ocariot-jwtScopes-policy'));
+    pluginContext.registerGatewayRoute(require('./routes/user.router-gateway'));
+
   },
   policies:['ocariot-jwt-policy', 'ocariot-jwtScopes-policy'], // this is for CLI to automatically add to "policies" whitelist in gateway.config
 };
