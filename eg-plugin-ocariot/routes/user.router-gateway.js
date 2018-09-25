d84d4dcd35f5fc9b527a23b2decb17aca2eb676b
andre.almeida <andre.almeida@nutes.uepb.edu.br>
2018-09-25 10:13:56 -0300

    EG-OCARIoT-Plugin update Implementing custom route to register user on API Gateway and Account Service

--- eg-plugin-ocariot/routes/user.router-gateway.js
+++ eg-plugin-ocariot/routes/user.router-gateway.js
@@ -1,5 +1,5 @@
 var bodyParser = require('body-parser');
-
+var userSrv = require('../services/consumers/user_service.js');
 module.exports = app => {
   const middlewares = [
     bodyParser.urlencoded({ extended: true }),
@@ -8,9 +8,12 @@ module.exports = app => {
 
   app.use('/api/v1/users', middlewares);
 
-  app.post('/api/v1/users',(req, res) => {
-    console.log('POST users: ' + JSON.stringify(req.body));
-    res.json({postUrl: req.url});
+  app.post('/api/v1/users', (req, res) => {
+    userSrv.createUser(req.body).then(user => {
+      res.status(201).send(user);
+    }).catch(err => {
+      res.status(409).send(err.message);
+    });
   });
 
   app.delete('/api/v1/users/:user_id',(req, res) => {
