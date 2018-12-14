09d1e11ffe041d56ccfcd60be2d08a44a7a3ea79
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2018-12-14 14:03:00 -0300

    Improvements in swagger UI
    Removed dependency of yamljs because it will no longer be used.
    yaml is loaded directly from swaggerhub.
    Inserted ico of ocariot.
    Inserted custom toolbar.

--- eg-plugin-ocariot/routes/reference-ocariot.js
+++ eg-plugin-ocariot/routes/reference-ocariot.js
@@ -1,17 +1,38 @@
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
 
-module.exports = function (expressGatewayApp) {    
-  const documentSwagger = YAML.load('./assets/swagger/nutes.ocariot-api-ocariot-v1.yaml');
+module.exports = function (expressGatewayApp) {
   const options = {
-    customCss: '.swagger-ui .topbar { display: none }',
-    customfavIcon: 'http://nutes.uepb.edu.br/wp-content/uploads/2014/01/icon.fw_.png',
+    swaggerUrl: 'https://api.swaggerhub.com/apis/nutes.ocariot/api-ocariot/v1/swagger.json',
+    customCss: `.swagger-ui .topbar { 
+                    background-color: #0097a7;  
+                    padding: 14px 0; 
+                }
+                .swagger-ui .topbar .topbar-wrapper a span { 
+                  display: none; 
+                }
+                .swagger-ui .topbar .topbar-wrapper a { 
+                    display: none;
+                }
+                .swagger-ui .topbar .topbar-wrapper:before { 
+                  content: url(http://www.ocariot.com.br/wp-content/uploads/2018/08/cropped-512-32x32.png);
+                  margin-left: -5px;
+              }
+                .swagger-ui .topbar .topbar-wrapper:after {  
+                    content: 'OCARIoT'; 
+                    margin: 0;
+                    padding: 0 10px;
+                    font-size: 1.7em;
+                    font-weight: 700;
+                    color: #fff; 
+                }`,
+    customfavIcon: 'http://www.ocariot.com.br/wp-content/uploads/2018/08/cropped-512-32x32.png',
     customSiteTitle: `API Reference | OCARIoT`
-  }  
+  }
   expressGatewayApp.get('/', (req, res) => {
     res.redirect('/v1/reference');
   });
+
   expressGatewayApp.use('/v1/reference', swaggerUi.serve, (req, res) => {
-    swaggerUi.setup(documentSwagger, options)(req, res);
-  });  
+    swaggerUi.setup(null, options)(req, res);
+  });
 };
