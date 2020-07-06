286d378c3b1024165dd84efa049fc9a76f9f29fc
Douglas Rafael <douglasrafaelcg@gmail.com>
2020-07-06 15:26:53 -0300

    Corrects the error of invalid iot clients and improves error messages.

--- express-gateway-plugin-ocariot/policies/device-authorization/authorization.js
+++ express-gateway-plugin-ocariot/policies/device-authorization/authorization.js
@@ -8,18 +8,24 @@ const msgInternalError = {
 module.exports = () => {
     return async (req, res, next) => {
         try {
+            // 1. Verify that a client certificate has been provided
             if (isEmpty(req.socket.getPeerCertificate(true))) {
-                return res.status(403).send('TLS client authentication failed!')
+                return res.status(400).send(handlerError(400))
             }
 
-            // 1. retrieving serialNumber and deviceId from certificate
+            // 2. Check if the client is authorized
+            if (!req.client.authorized) {
+                return res.status(403).send(handlerError(403))
+            }
+
+            // 3. retrieving serialNumber and deviceId from certificate
             const deviceId = req.socket.getPeerCertificate(true).subject.CN
             const serialNumber = req.socket.getPeerCertificate(true).serialNumber
             const device = await deviceDao.getCertInfo(deviceId)
 
-            // 2. comparing the certificate's serialNumber with the serialNumber maintained at the gateway
+            // 3.1. comparing the certificate's serialNumber with the serialNumber maintained at the gateway
             if (!device || device.serial_number.replace(/:/g, '') !== serialNumber.toLowerCase()) {
-                return errorHandler(403, res)
+                return res.status(403).send(handlerError(403))
             }
 
             next()
@@ -31,4 +37,30 @@ module.exports = () => {
 
 function isEmpty(obj) {
     return Object.keys(obj).length === 0
+}
+
+function handlerError(code) {
+    const messages = {
+        400: () => {
+            return {
+                code: 400,
+                message: 'TLS client authentication failed!',
+                description: 'Client certificate are required!'
+            }
+        },
+        403: () => {
+            return {
+                code: 403,
+                message: 'TLS client authorization failed!',
+                description: 'Client certificate is not valid or has been revoked!'
+            }
+        },
+        500: () => {
+            return {
+                code: 500,
+                message: 'An internal server error has occurred.'
+            }
+        }
+    }
+    return (messages[code] || messages[500])()
 }
