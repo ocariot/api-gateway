48136fa2299781fa0acb8ccf4afb923cc2f3ab31
lucas-barbosa-oliveira <lucas.barbosa@nutes.uepb.edu.br>
2020-08-15 12:33:55 -0300

    Check the association of the device with the institution before recording the measurements of the environment

--- express-gateway-plugin-ocariot/policies/device-authorization/authorization.js
+++ express-gateway-plugin-ocariot/policies/device-authorization/authorization.js
@@ -1,45 +1,79 @@
-const errorHandler = require('./../../utils/error.handler')
+const generalErrorHandler = require('./../../utils/error.handler')
 const deviceDao = require('./../device-pki/pki.dao')
+const service = require('../../utils/http-client')
 
-const msgInternalError = {
-    code: 500, message: 'An operation could not be completed due to an internal error...'
-}
-
-module.exports = () => {
+let IOT_TRACKING_SERVICE = ''
+module.exports = function (actionParams) {
+    IOT_TRACKING_SERVICE = actionParams.iotTrackingUrlBase
     return async (req, res, next) => {
-        try {
-            // 1. Verify that a client certificate has been provided
-            if (isEmpty(req.socket.getPeerCertificate(true))) {
-                return res.status(400).send(handlerError(400))
-            }
 
-            // 2. Check if the client is authorized
-            if (!req.client.authorized) {
-                return res.status(403).send(handlerError(403))
-            }
+        /**
+         * ####### CHILDREN.WEIGHTS #######
+         * ####### ENVIRONMENTS #######
+         */
+        // POST /v1/children/{user_name}/weights ['measurements:create']
+        // POST /v1/institutions/{institution_id}/environments ['environment:create']
+        await tlsAuthorization(req, res)
 
-            // 3. retrieving serialNumber and deviceId from certificate
-            const deviceId = req.socket.getPeerCertificate(true).subject.CN
-            const serialNumber = req.socket.getPeerCertificate(true).serialNumber
-            const device = await deviceDao.getCertInfo(deviceId)
-
-            // 3.1. comparing the certificate's serialNumber with the serialNumber maintained at the gateway
-            if (!device || device.serial_number.replace(/:/g, '') !== serialNumber.toLowerCase()) {
-                return res.status(403).send(handlerError(403))
-            }
-
+        /**
+         * ####### ENVIRONMENTS #######
+         */
+        // POST /v1/institutions/{institution_id}/environments ['environment:create']
+        if (/^((\/v1\/institutions\/)[^\W_]{24}\/environments\/{0,1})$/.test(req.path) && req.method === 'POST') {
+            checkDeviceAssociation(req, res, next)
+        } else {
             next()
-        } catch (e) {
-            res.status(msgInternalError.code).json(msgInternalError)
         }
     }
+}
+
+async function tlsAuthorization(req, res) {
+    try {
+        // 1. Verify that a client certificate has been provided
+        if (isEmpty(req.socket.getPeerCertificate(true))) {
+            return res.status(400).send(errorHandler(400))
+        }
+
+        // 2. Check if the client is authorized
+        if (!req.client.authorized) {
+            return res.status(403).send(errorHandler(403))
+        }
+
+        // 3. retrieving serialNumber and deviceId from certificate
+        const deviceId = req.socket.getPeerCertificate(true).subject.CN
+        const serialNumber = req.socket.getPeerCertificate(true).serialNumber
+        const device = await deviceDao.getCertInfo(deviceId)
+
+        // 3.1. comparing the certificate's serialNumber with the serialNumber maintained at the gateway
+        if (!device || device.serial_number.replace(/:/g, '') !== serialNumber.toLowerCase()) {
+            return res.status(403).send(errorHandler(403))
+        }
+    } catch (e) {
+        res.status(500).send(errorHandler(500))
+    }
+}
+
+function checkDeviceAssociation(req, res, next) {
+    const deviceId = req.socket.getPeerCertificate(true).subject.CN
+    const institutionId = req.params.institution_id
+
+    service
+        .get(IOT_TRACKING_SERVICE.concat(`/v1/institutions/${institutionId}/devices/${deviceId}`))
+        .then(result => {
+            if (result.status !== 200) return res.status(403).send(generalErrorHandler(403, res))
+            next()
+        })
+        .catch((e) => {
+            if (!e.response) return res.status(500).send(errorHandler(500))
+            return res.status(403).send(generalErrorHandler(403, res))
+        })
 }
 
 function isEmpty(obj) {
     return Object.keys(obj).length === 0
 }
 
-function handlerError(code) {
+function errorHandler(code) {
     const messages = {
         400: () => {
             return {
